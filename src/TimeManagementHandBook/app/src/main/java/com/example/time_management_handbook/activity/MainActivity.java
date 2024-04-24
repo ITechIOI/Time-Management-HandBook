8194454c541c2f5582738336bbd4372eb47550fc
AkumaP41 <22521094@gm.uit.edu.vn>
2024-04-25 02:15:40 +0700

    Update and change first appearance to MainActivity

--- src/TimeManagementHandBook/app/src/main/java/com/example/time_management_handbook/activity/MainActivity.java
+++ src/TimeManagementHandBook/app/src/main/java/com/example/time_management_handbook/activity/MainActivity.java
@@ -29,14 +29,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        Intent intent = new Intent(getApplicationContext(), Login_Activity.class);
-        startActivity(intent);
-        finish();
-        //welcomeScreen();
+        welcomeScreen();
     }
 
     private void welcomeScreen(){
-        int time = 1000;
+        int time = 1000; //Thời gian screen welcome xuất hiện
         new Handler().postDelayed(()->{
             Intent intent = new Intent(getApplicationContext(), Login_Activity.class);
             startActivity(intent);

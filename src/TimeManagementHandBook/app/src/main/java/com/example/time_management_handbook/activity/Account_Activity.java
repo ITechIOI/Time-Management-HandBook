74b872a5ee84b09687e1f5d3e653f01efa4d9b7d
AkumaP41 <22521094@gm.uit.edu.vn>
2024-04-25 06:56:59 +0700

    Update Java & UI for Account Activity (except icon)

---
+++ src/TimeManagementHandBook/app/src/main/java/com/example/time_management_handbook/activity/Account_Activity.java
@@ -0,0 +1,51 @@
+package com.example.time_management_handbook.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.time_management_handbook.R;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+public class Account_Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+
+        Button addAccount = findViewById(R.id.button_AddAccount);
+        addAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BottomSheetDialog bsd = new BottomSheetDialog(Account_Activity.this);
+                View view = LayoutInflater.from(Account_Activity.this).inflate(R.layout.fragment_add_account_, null);
+                bsd.setContentView(view);
+                bsd.show();
+
+                // Thiết lập khi nhấn các item của listView (tài khoản có sẵn)
+
+                Button addNewAccount = view.findViewById(R.id.button_AddNewAccount);
+                addNewAccount.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent mit= new Intent(Account_Activity.this, Login_Activity.class);
+                        startActivity(mit);
+                    }
+                });
+            }
+        });
+
+        Button logout = findViewById(R.id.button_Logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mit= new Intent(Account_Activity.this, Login_Activity.class);
+                startActivity(mit);
+            }
+        });
+    }
+}
--- src/TimeManagementHandBook/app/src/main/java/com/example/time_management_handbook/activity/Home_Activity.java
+++ src/TimeManagementHandBook/app/src/main/java/com/example/time_management_handbook/activity/Home_Activity.java
@@ -8,9 +8,11 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -30,6 +32,8 @@ public class Home_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Toolbar toolbar=findViewById(R.id.home_toolbar);
         setSupportActionBar(toolbar);
+        // Tạo logo
+        // getSupportActionBar().setLogo(R.drawable.google); // Chưa kịp kiếm hình
 
         FloatingActionButton fab = findViewById(R.id.float_button_add);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -83,6 +87,23 @@ public class Home_Activity extends AppCompatActivity {
         });
 
     }
+
+    // 2 hàm tạo kết nối với account button
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (item.getItemId() == R.id.item_avatar){
+            Intent mit= new Intent(Home_Activity.this, Account_Activity.class);
+            startActivity(mit);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private  void loadFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();

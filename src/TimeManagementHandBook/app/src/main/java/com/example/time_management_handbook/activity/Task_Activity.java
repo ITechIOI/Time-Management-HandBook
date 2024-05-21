fc9b86dfa5b2b6ebd6f002368b3268d65aea5d9b
PhuongHo105 <22521160@gm.uit.edu.vn>
2024-05-21 09:32:12 +0700

    Update Task UI

--- src/TimeManagementHandBook/app/src/main/java/com/example/time_management_handbook/activity/Task_Activity.java
+++ src/TimeManagementHandBook/app/src/main/java/com/example/time_management_handbook/activity/Task_Activity.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -111,6 +112,15 @@ public class Task_Activity extends AppCompatActivity {
                 orange.setChecked(true);
                 break;
         }
+        TextInputEditText finishTime = findViewById(R.id.taFinishTime_textInput);
+        CheckBox isFinished = findViewById(R.id.taFinished_checkbox);
+        if (task.getFinishedTime()!=null)
+        {
+            finishTime.setText(task.getFinishedTime().toString());
+            isFinished.setChecked(true);
+        }
+        else
+            isFinished.setChecked(false);
 
         deadlineImageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -175,6 +185,8 @@ public class Task_Activity extends AppCompatActivity {
                 }
             }
         });
+
+
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
